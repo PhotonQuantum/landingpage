2a34a54baca989fdd41df56e07e3a953aff585c3
LightQuantum <self@lightquantum.me>
2024-07-31 23:13:27 +0800

    Update contact

--- components/MailTo.tsx
+++ components/MailTo.tsx
@@ -8,7 +8,10 @@ export const MailTo = ({ template }) => {
     let screenname = document.querySelector(".sn").textContent.toLowerCase();
     let realname = document.querySelector(".rn").textContent.toLowerCase().replaceAll(" ", "");
     let realnamedot = document.querySelector(".rn").textContent.toLowerCase().replaceAll(" ", ".");
-    setEmailAddress(template.replaceAll("{realname}", realname).replaceAll("{realnamedot}", realnamedot).replaceAll("{screenname}", screenname));
+    let firstname = document.querySelector(".rn").textContent.toLowerCase().split(" ")[0];
+    let lastname = document.querySelector(".rn").textContent.toLowerCase().split(" ")[1];
+    setEmailAddress(template.replaceAll("{realname}", realname).replaceAll("{realnamedot}", realnamedot).replaceAll("{screenname}", screenname)
+      .replaceAll("{firstname}", firstname).replaceAll("{lastname}", lastname));
   }, [template]);
   return (
     <a href={`mailto:${emailAddress}`}>{emailAddress}</a>
