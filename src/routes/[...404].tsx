6e0fec8ffdcf1e7abbbe2126bf501ade4b27c34c
LightQuantum <self@lightquantum.me>
2026-02-21 20:46:05 -0500

    fix: 404 page returns 200 status code

--- src/routes/[...404].tsx
+++ src/routes/[...404].tsx
@@ -1,8 +1,10 @@
+import { HttpStatusCode } from "@solidjs/start";
 import SvgGhost3 from "@tabler/icons/outline/ghost-3.svg"
 
 export default function NotFound() {
   return (
     <main class="text-center mx-auto my-auto text-strong flex items-center gap-2">
+      <HttpStatusCode code={404} />;
       <SvgGhost3 class="w-20 h-20 stroke-[0.8]" />
       <div class="text-start flex flex-col gap-1">
         <h1 class="text-2xl font-semibold">404</h1>
